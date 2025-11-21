c6aaf84eb8359e25d2283ccb7ac592d85f51afc2
Sal Khalil <sal.khalil@live.com>
2025-11-21 16:17:40 +0000

    enh: Update prisma schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,25 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+  pool: Pool | undefined;
+};
+
+const pool =
+  globalForPrisma.pool ??
+  new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
-  })
+    adapter,
+    log: ["query"],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-
-
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+  globalForPrisma.pool = pool;
+}
