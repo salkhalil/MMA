3c82caca387c55ed2b18a6365cbf450a90bbac6e
Sal Khalil <sal.khalil@live.com>
2025-11-21 16:56:50 +0000

    fix the tings

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,25 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
-  pool: Pool | undefined;
 };
-
-const pool =
-  globalForPrisma.pool ??
-  new Pool({ connectionString: process.env.DB_PRISMA_DATABASE_URL });
-const adapter = new PrismaPg(pool);
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
-    log: ["query"],
+    accelerateUrl: process.env.DB_PRISMA_DATABASE_URL,
   });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
-  globalForPrisma.pool = pool;
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import "dotenv/config";
+import { prisma } from "../lib/prisma";
 
 async function main() {
   // Add your friends' names here
